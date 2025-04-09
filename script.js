15896eeafa71e607c2e8e2fa0bc03b40121bb2e6
Arif Momin <arifmominweb@gmail.com>
2025-04-09 19:46:03 +0600

    zsdfgvds

--- script.js
+++ script.js
@@ -1,7 +1,35 @@
-function openPopup(imgElement) {
-    document.getElementById('popup-img').src = imgElement.src;
-    document.getElementById('popup').style.display = 'flex';
+function smallDevice (){
+    if(window.innerWidth <= 720){
+        const calculateTitle = document.querySelector('.calculate-title');
+        calculateTitle.addEventListener('click', () =>{
+            calculateTitle.classList.toggle('active');
+        })
+    }
 }
-function closePopup() {
-    document.getElementById('popup').style.display = 'none';
+smallDevice();
+ $(document).ready(function () {
+    if(window.innerWidth < 720){
+        $('.calculate-title').on('click', function() {
+        $('.calculator-description').toggle();
+        $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
+    });
+    };
+});
+
+function estimateFiO2(flow) {
+    let fio2 = 20 + (flow * 4);
+    return fio2.toFixed(1);
 }
+
+function updateFiO2() {
+    let flow = parseFloat($('#flow').val()).toFixed(1);
+    $('#flowVal').text(flow);
+
+    let fio2 = estimateFiO2(flow);
+    $('#fio2').text(fio2 + '%');
+}
+
+$(document).ready(function () {
+    updateFiO2();
+    $('#flow').on('input', updateFiO2);
+});
