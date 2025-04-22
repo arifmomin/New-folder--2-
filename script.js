6cae38aa394c15dfe9b5fd4619176724ad16bbe5
Arif Momin <arifmominweb@gmail.com>
2025-04-22 20:53:54 +0600

    sdfsdf

--- script.js
+++ script.js
@@ -1,35 +1,60 @@
-function smallDevice (){
-    if(window.innerWidth <= 720){
-        const calculateTitle = document.querySelector('.calculate-title');
-        calculateTitle.addEventListener('click', () =>{
-            calculateTitle.classList.toggle('active');
-        })
+
+
+ const menu = document.querySelector(".menu");
+ const menuMain = menu.querySelector(".menu-main");
+ const goBack = menu.querySelector(".go-back");
+ const menuTrigger = document.querySelector(".mobile-menu-trigger");
+ const closeMenu = menu.querySelector(".mobile-menu-close");
+ let subMenu;
+ menuMain.addEventListener("click", (e) =>{
+  if(!menu.classList.contains("active")){
+    return;
+  }
+   if(e.target.closest(".menu-item-has-children")){
+     const hasChildren = e.target.closest(".menu-item-has-children");
+      showSubMenu(hasChildren);
+   }
+ });
+ goBack.addEventListener("click",() =>{
+   hideSubMenu();
+ })
+ menuTrigger.addEventListener("click",() =>{
+   toggleMenu();
+ })
+ closeMenu.addEventListener("click",() =>{
+   toggleMenu();
+ })
+ document.querySelector(".menu-overlay").addEventListener("click",() =>{
+  toggleMenu();
+ })
+ function toggleMenu(){
+  menu.classList.toggle("active");
+  document.querySelector(".menu-overlay").classList.toggle("active");
+ }
+ function showSubMenu(hasChildren){
+    subMenu = hasChildren.querySelector(".sub-menu");
+    subMenu.classList.add("active");
+    subMenu.style.animation = "slideLeft 0.5s ease forwards";
+    const menuTitle = hasChildren.querySelector("i").parentNode.childNodes[0].textContent;
+    menu.querySelector(".current-menu-title").innerHTML=menuTitle;
+    menu.querySelector(".mobile-menu-head").classList.add("active");
+ }
+
+ function  hideSubMenu(){  
+    subMenu.style.animation = "slideRight 0.5s ease forwards";
+    setTimeout(() =>{
+       subMenu.classList.remove("active");  
+    },300); 
+    menu.querySelector(".current-menu-title").innerHTML="";
+    menu.querySelector(".mobile-menu-head").classList.remove("active");
+ }
+ 
+ window.onresize = function(){
+  if(this.innerWidth >991){
+    if(menu.classList.contains("active")){
+      toggleMenu();
     }
-}
-smallDevice();
- $(document).ready(function () {
-    if(window.innerWidth < 720){
-        $('.calculate-title').on('click', function() {
-        $('.calculator-description').toggle();
-        $(this).find('i').toggleClass('fa-angle-down fa-angle-up');
-    });
-    };
-});
 
-function estimateFiO2(flow) {
-    let fio2 = 20 + (flow * 4);
-    return fio2.toFixed(1);
-}
+  }
+ }
 
-function updateFiO2() {
-    let flow = parseFloat($('#flow').val()).toFixed(1);
-    $('#flowVal').text(flow);
-
-    let fio2 = estimateFiO2(flow);
-    $('#fio2').text(fio2 + '%');
-}
-
-$(document).ready(function () {
-    updateFiO2();
-    $('#flow').on('input', updateFiO2);
-});
